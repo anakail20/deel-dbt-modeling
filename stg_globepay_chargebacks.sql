ee1d44fb9021c7fc549664c428ad20fd64a8fede
anakail20 <anastasiakailani@gmail.com>
2025-02-01 15:54:19 +0100

    Delete stg_globepay_chargebacks.sql

--- stg_globepay_chargebacks.sql
+++
@@ -1,8 +0,0 @@
-WITH raw_chargebacks AS (
-    SELECT
-        external_ref,
-        CAST(chargeback AS BOOLEAN) AS chargeback_flag
-    FROM {{ source('globepay', 'chargebacks') }}
-)
-
-SELECT * FROM raw_chargebacks
