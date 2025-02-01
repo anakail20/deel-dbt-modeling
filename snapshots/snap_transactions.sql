be7738ed5d34c86b9a1c8a0f830f35b98d1b8b19
anakail20 <anastasiakailani@gmail.com>
2025-02-01 16:43:07 +0100

    Add files via upload

---
+++ snapshots/snap_transactions.sql
@@ -0,0 +1,34 @@
+{% snapshot snap_transactions %}
+
+/*
+Purpose:
+- Tracks changes in transaction status (state) and chargeback events over time.
+- Helps monitor updates to transactions for auditing, reporting, and fraud detection.
+
+How it works:
+- Uses `strategy='timestamp'` to capture changes based on date_time.
+- Stores historical snapshots whenever stat` or chargeback status changes.
+*/
+
+{{ 
+    config(
+        target_schema='snapshots',
+        unique_key='external_ref',  -- Ensures each transaction is uniquely tracked
+        strategy='timestamp',       -- Captures changes based on `date_time`
+        updated_at='date_time'      -- Detects updates in the transaction record
+    ) 
+}}
+
+SELECT 
+    external_ref,       
+    date_time,          
+    state,              
+    chargeback,         
+    amount,             
+    currency,          
+    country,           
+    rates,              
+    merchantReference   
+FROM {{ source('staging', 'stg_globepay_transactions') }}
+
+{% endsnapshot %}
