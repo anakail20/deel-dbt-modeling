4592dc30768dcea034aa019b1f0d3c7eaf9c2d65
anakail20 <anastasiakailani@gmail.com>
2025-02-01 16:55:35 +0100

    Add files via upload

---
+++ models/staging/stg_globepay_acceptances.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+
+WITH raw_acceptance AS (
+   SELECT
+        external_ref,
+        ref AS transaction_id,
+        state,
+        cvv_provided::BOOLEAN AS cvv_used,
+        {{ format_currency('amount') }} AS transaction_amount,
+        country,
+        currency,
+        {{ convert_timestamp('date_time') }} AS transaction_date,
+        {{ extract_json_value('rates', 'USD') }} AS exchange_rate_to_usd
+    FROM {{ source('globepay', 'acceptance') }}
+)
+
+SELECT * FROM raw_acceptance;
