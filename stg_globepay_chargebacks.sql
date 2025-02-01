a35b605c8a6e4cad89a6f7f534dffe8bf7c9e210
anakail20 <anastasiakailani@gmail.com>
2025-02-01 15:53:20 +0100

    Add stg

---
+++ stg_globepay_chargebacks.sql
@@ -0,0 +1,8 @@
+WITH raw_chargebacks AS (
+    SELECT
+        external_ref,
+        CAST(chargeback AS BOOLEAN) AS chargeback_flag
+    FROM {{ source('globepay', 'chargebacks') }}
+)
+
+SELECT * FROM raw_chargebacks
